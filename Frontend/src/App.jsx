405a2489646f54cc34a6f1eea617bad74679a25b
Zirwa Abid <zirwaabid112@gmail.com>
2025-07-02 14:05:39 +0500

    react

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Doctor from './pages/Doctor'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointment from './Appointment'
+import NavBar from './components/NavBar'
 
-export default function App() {
+const App = () => {
   return (
-    <div></div>
+    <div className='mx-4 sm:mx-[10%]'>
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/doctors' element={<Doctor />} />
+        <Route path='/doctors/:speciality' element={<Doctor />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/appointement/:docId' element={<Appointment />} />
+      </Routes>
+    </div>
   )
-}
+};
+
+export default App

4e31e6b83a8b30ef57dc9a826a077451c9a9e5e4
Zirwa Abid <zirwaabid112@gmail.com>
2025-08-13 14:39:17 +0500

    backend

---
+++ Backend/controllers/adminController.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+const addDoctor=async()=>{
+    try {
+        const {name,email,password,speciality,degree,experience,about,fees,address}=req.body
+    } catch (error) {
+        
+    }
+}
